b48510604e224b29e254187468a967a8ef755f88
Sayaka Ono <sayaka.ono.81@gmail.com>
2019-04-26 20:57:30 -0700

    install mongoose

--- controllers/todocontroller.js
+++ controllers/todocontroller.js
@@ -1,22 +1,47 @@
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
-var data = [{ item: 'get milk' }, { item: 'walk dog' }, { item: 'exercise' }];
+var data = require('../mongodb');
+
+mongoose.connect(data, { useNewUrlParser: true });
+
+var todoSchema = new mongoose.Schema({
+  item: String
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+
+// var data = [{ item: 'get milk' }, { item: 'walk dog' }, { item: 'exercise' }];
 var urlencodeParser = bodyParser.urlencoded({ extended: false });
 
 module.exports = function(app) {
   app.get('/todo', function(req, res) {
-    res.render('todo', { todos: data });
+    Todo.find({}, function(err, data) {
+      if (err) {
+        throw err;
+      }
+      res.render('todo', { todos: data });
+    });
   });
 
   app.post('/todo', urlencodeParser, function(req, res) {
-    data.push(req.body);
-    res.json(data);
+    var newTodo = Todo(req.body).save(function(err, data) {
+      if (err) {
+        throw err;
+      }
+      res.json(data);
+    });
   });
 
   app.delete('/todo/:item', function(req, res) {
-    data = data.filter(function(todo) {
-      return todo.item.replace(/ /g, '-') !== req.params.item;
+    Todo.find({ item: req.params.item.replace(/\-/g, ' ') }).deleteOne(function(
+      err,
+      data
+    ) {
+      if (err) {
+        throw err;
+      }
+      res.json(data);
     });
-    res.json(data);
   });
 };
